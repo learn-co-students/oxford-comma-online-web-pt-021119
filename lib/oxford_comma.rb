74b2aa05dd0c307301513bc605766526a93a85ae
Katie Blizzard <kblizzard11@gmail.com>
2019-02-20 22:41:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,40 @@
-def oxford_comma(array)
+def oxford_comma(array) 
 
-end
+  case array.size  
+
+    when 1  
+
+      return array[0] 
+
+    when 2  
+
+      return array.join(" and ") 
+
+    when 3  
+
+      return "#{array[0]}, #{array[1]}, and #{array[2]}" 
+
+  end  
+
+   
+
+  if array.size > 3 
+
+    message = "" 
+
+    string1 = array[0..-2].join(", ")
+     
+    string2 = array.last 
+
+    message << string1
+
+    message << ", and #{string2}" 
+    
+    return message
+
+   end 
+
+end 
+
+  
+
