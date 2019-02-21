bc5bdb642507d342c8168755343aa16ebf655954
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-21 03:19:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array) 
+if array.length == 1 
+  return array.join
+elsif array.length == 2 
+  return array.join(" and ")
+else array.length >= 3
+  return array[-1].insert(0, " and ")
+  array.join(" , ")
 end
