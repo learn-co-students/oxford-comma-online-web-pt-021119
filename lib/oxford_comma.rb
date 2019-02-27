7a4fdb9e2b7cb7fe10b4f67677cc604a2b6004f3
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-02-27 04:16:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array.insert(-2, "and")
+    # array.join(-2..-1)
+    array.join(" ")
+  elsif array.length >= 3
+    array.insert(-2, "and")
+    firstpart = array[0..-2].join(", ")
+    secondpart = firstpart + " " + array[-1]
+    else
+      return array.join
+    end
 end
